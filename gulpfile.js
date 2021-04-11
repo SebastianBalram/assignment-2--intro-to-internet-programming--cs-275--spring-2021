9007c8afd71ac91501e9d8036a25732a9785564e
SebastianBalram <sebastian.balram10@gmail.com>
2021-04-11 17:46:17 -0400

    add basic HTMLvalidator task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,5 @@
-const { html, prod, series } = require(`gulp`);
-const htmlValidator = require(`gulp-html`);
-const htmlCompressor = require(`gulp-htmlmin`);
+const { src } = require(`gulp`);
 let validateHTML = () => {
-    return html(`html/*.html`)
-        .pipe(htmlValidator());
-};
-
-let compressHTML = () => {
-    return src(`html/*.html`)
-        .pipe(htmlCompressor({collapseWhitespace: true}))
-        .pipe(dest(`prod/`));
-};
-
-exports.validateHTML = validateHTML;
-exports.compressHTML = compressHTML;
-exports.runAll = series(validateHTML,compressHTML);
+    return src([`html/*.html`, `html/**/*.html`]).pipe(htmlValidator());
+ };
+ exports.validateHTML = validateHTML;
