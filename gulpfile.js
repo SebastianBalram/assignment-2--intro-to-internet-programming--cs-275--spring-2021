c13fabbec4eafd999072f2103852b2dd77f6b2fc
SebastianBalram <sebastian.balram10@gmail.com>
2021-04-13 13:33:04 -0400

    added eslint and babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,56 @@
-const { src } = require(`gulp`);
+const { src, dest, series } = require(`gulp`);
+const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const jsLinter = require(`gulp-eslint`);
+const babel = require(`gulp-babel`);
+
 let validateHTML = () => {
     return src([`html/*.html`, `html/**/*.html`]).pipe(htmlValidator());
- };
- exports.validateHTML = validateHTML;
+};
+
+let compressHTML = () => {
+    return src([`html/*.html`,`html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let lintJS = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(jsLinter({
+            parserOptions: {
+                ecmaVersion: 2017,
+                sourceType: `module`
+            },
+            rules: {
+                indent: [2, 4, {SwitchCase: 1}],
+                quotes: [2, `backtick`],
+                semi: [2, `always`],
+                'linebreak-style': [2, `unix`],
+                'max-len': [1, 85, 4]
+            },
+            env: {
+                es6: true,
+                node: true,
+                browser: true
+            },
+            extends: `eslint:recommended`
+        }))
+        .pipe(jsLinter.formatEach(`compact`, process.stderr));
+};
+let transpileJSForDev = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/scripts`));
+};
+let transpileJSForProd = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
+exports.lintJS = lintJS;
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.HTMLProcessing = series(validateHTML, compressHTML);
+exports.transpileJSForDev = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
